64416d028e84a27d1c19298f5f266e6ef749a76f
andreiOpran <andrei.opran@icloud.com>
2026-04-07 20:07:39 +0300

    [FIX] useless code that was moved previously

--- operational-node/internal/jobs/cron_pipeline.go
+++ operational-node/internal/jobs/cron_pipeline.go
@@ -8,24 +8,6 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-// SyncPayload represents the JSON payload to send for CMD_SYNC
-type SyncPayload struct {
-	EquityTickers []string `json:"equity_tickers"`
-	BondTickers   []string `json:"bond_tickers"`
-}
-
-// GeneratePayload represents the JSON payload to send for CMD_GENERATE
-type GeneratePayload struct {
-	EquityTickers      []string           `json:"equity_tickers"`
-	BondTickers        []string           `json:"bond_tickers"`
-	MacroAllocations   map[int]float64    `json:"macro_allocations"`
-	HorizonMultipliers map[string]float64 `json:"horizon_multipliers"`
-	MaxEquityCap       float64            `json:"max_equity_cap"`
-	TopNEquities       int                `json:"top_n_equities"`
-	WeightThreshold    float64            `json:"weight_threshold"`
-	Verbose            bool               `json:"verbose"`
-}
-
 // StartDataPipelineJob schedules the CMD_SYNC and CMD_GENERATE messages
 func StartDataPipelineJob(pipelineService services.DataPipelineService) {
 	c := cron.New()
